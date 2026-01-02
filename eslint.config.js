33940e1b64cf9a6018dbf935fda236e08cfe3b99
HwangGoeun <hwang_gim@naver.com>
2026-01-02 12:05:58 +0900

    chore: 프론트엔트 초기 개발 환경 세팅

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      'simple-import-sort': simpleImportSort,
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      // Import 순서 규칙
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      
+      // var 사용 금지
+      'no-var': 'error',
+      'prefer-const': 'error',
+      
+      // 코드 스타일
+      'semi': ['error', 'always'],
+      'indent': ['error', 2, { SwitchCase: 1 }],
+      'eol-last': ['error', 'always'],
+      'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+      'jsx-quotes': ['error', 'prefer-double'],
+      
+      // TypeScript 규칙
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_' 
+      }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-var-requires': 'error',
+      
+      // React 규칙
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+    },
+  },
+])
