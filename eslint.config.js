eea7a353bcfd0ddf73b3e2c929bf4e547fb373ff
HwangGoeun <hwang_gim@naver.com>
2026-01-06 18:05:02 +0900

    chore: Shadcn/ui 컴포넌트 라이브러리 추가

--- eslint.config.js
+++ eslint.config.js
@@ -36,33 +36,38 @@ export default defineConfig([
       // Import 순서 규칙
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      
+
       // var 사용 금지
       'no-var': 'error',
       'prefer-const': 'error',
-      
+
       // 코드 스타일
       'semi': ['error', 'always'],
       'indent': ['error', 2, { SwitchCase: 1 }],
       'eol-last': ['error', 'always'],
       'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
       'jsx-quotes': ['error', 'prefer-double'],
-      
+
       // TypeScript 규칙
-      '@typescript-eslint/no-unused-vars': ['error', { 
+      '@typescript-eslint/no-unused-vars': ['error', {
         argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_' 
+        varsIgnorePattern: '^_'
       }],
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-var-requires': 'error',
-      
+
       // React 규칙
       'react/prop-types': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/jsx-uses-react': 'off',
       'react/jsx-uses-vars': 'error',
+
+      // React Refresh 규칙 - shadcn/ui 패턴 허용
+      'react-refresh/only-export-components': ['warn', {
+        allowConstantExport: true
+      }],
     },
   },
 ])
