dbf8d38ce4840ef8b4b04a7b7ab116758874ad3f
HwangGoeun <hwang_gim@naver.com>
2026-01-24 20:04:29 +0900

    feat: 템플릿 자동 저장 기능 구현 및 상태 표시 UI 추가
    - 1초 디바운싱을 적용한 자동 저장 기능 구현
    - 페이지 이탈 방지 로직 분리
    - 템플릿 헤더에 실시간 저장 상태(작성 중, 저장됨 등) 표시

---
+++ src/hooks/use-before-unload.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+export function useBeforeUnload(shouldPrevent: () => boolean) {
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      if (shouldPrevent()) {
+        e.preventDefault();
+        e.returnValue = '';
+      }
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => window.removeEventListener('beforeunload', handleBeforeUnload);
+  }, [shouldPrevent]);
+}
